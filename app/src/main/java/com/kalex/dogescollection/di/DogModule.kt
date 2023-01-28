2e8363fa0de1daf0ab13082ae6d7a0248bf673e4
Kevin Soto <kevinalexandersoto999@gmail.com>
2023-01-28 15:20:41 -0500

    feature : add Hilt mdule and retrofit bilder to di

---
+++ app/src/main/java/com/kalex/dogescollection/di/DogModule.kt
@@ -0,0 +1,23 @@
+package com.kalex.dogescollection.di
+
+import com.kalex.dogescollection.common.Constants
+import com.kalex.dogescollection.dogList.model.data.DogsApi
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DogModule {
+
+    @Provides
+    @Singleton
+    fun provideDogApi(): DogsApi = Retrofit.Builder()
+        .baseUrl(Constants.BASE_URL)
+        .addConverterFactory(MoshiConverterFactory.create())
+        .build().create(DogsApi::class.java)
+}
