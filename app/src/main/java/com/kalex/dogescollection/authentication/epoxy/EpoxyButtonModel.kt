99549f431a317519e58a9f9b0832aa57372e6254
Kevin Soto <kevinalexandersoto999@gmail.com>
2023-02-10 12:28:55 -0500

    feature : add isEnabled logic to the epoxy button

--- app/src/main/java/com/kalex/dogescollection/authentication/epoxy/EpoxyButtonModel.kt
+++ app/src/main/java/com/kalex/dogescollection/authentication/epoxy/EpoxyButtonModel.kt
@@ -1,5 +1,6 @@
 package com.kalex.dogescollection.authentication.epoxy
 
+import android.util.MutableBoolean
 import com.airbnb.epoxy.EpoxyAttribute
 import com.airbnb.epoxy.EpoxyModelClass
 import com.airbnb.epoxy.EpoxyModelWithHolder
@@ -8,12 +9,20 @@ import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textfield.TextInputLayout
 import com.kalex.dogescollection.R
 import com.kalex.dogescollection.common.epoxyhelpers.KotlinEpoxyHolder
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.collectLatest
 
 @EpoxyModelClass(layout = R.layout.authentication_buttom)
 abstract class EpoxyButtonModel : EpoxyModelWithHolder<EpoxyButtonModel.Holder>() {
 
     @EpoxyAttribute
     lateinit var buttonText : String
+
+    @EpoxyAttribute
+    var initialButtonEnableState : Boolean = false
+
+    @EpoxyAttribute
+    lateinit var enableButton : (button : MaterialButton) -> Unit
 
     @EpoxyAttribute
     lateinit var onClickListener : () -> Unit
@@ -23,12 +32,16 @@ abstract class EpoxyButtonModel : EpoxyModelWithHolder<EpoxyButtonModel.Holder>(
 
     override fun bind(holder: Holder) {
         super.bind(holder)
-        holder.buttonView.text = buttonText
-        holder.buttonView.setTextAppearance(textStyle)
-        holder.buttonView.setOnClickListener{
-            onClickListener
-        }
 
+        with(holder){
+            buttonView.text = buttonText
+            buttonView.setTextAppearance(textStyle)
+            buttonView.isEnabled = initialButtonEnableState
+            enableButton.invoke(holder.buttonView)
+            buttonView.setOnClickListener{
+                onClickListener.invoke()
+            }
+        }
     }
     inner class Holder():  KotlinEpoxyHolder(){
         val buttonView by bind<MaterialButton>(R.id.authenticationButton)
--- app/src/main/java/com/kalex/dogescollection/authentication/login/presentation/ui/LoginFragment.kt
+++ app/src/main/java/com/kalex/dogescollection/authentication/login/presentation/ui/LoginFragment.kt
@@ -8,7 +8,12 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import android.widget.Toast.LENGTH_LONG
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.fragment.findNavController
+import com.airbnb.epoxy.DiffResult
+import com.google.android.material.button.MaterialButton
 import com.kalex.dogescollection.authentication.epoxy.epoxyButton
 import com.kalex.dogescollection.authentication.epoxy.epoxyInputField
 import com.kalex.dogescollection.authentication.epoxy.epoxyInputPassword
@@ -16,6 +21,9 @@ import com.kalex.dogescollection.authentication.epoxy.epoxyTextButton
 import com.kalex.dogescollection.authentication.epoxy.epoxyTextTitle
 import com.kalex.dogescollection.databinding.FragmentLoginBinding
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class LoginFragment : Fragment() {
@@ -33,6 +41,9 @@ class LoginFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+         var enableuser = false
+         var enablepass = false
+         val state = MutableStateFlow(false)
         binding.loginEpoxyRecyclerView.withModels {
             epoxyTextTitle{
                 id(0)
@@ -43,10 +54,17 @@ class LoginFragment : Fragment() {
                 //TODO: Add strings resources
                 textHint("Usuario")
                 regexValidation( Patterns.EMAIL_ADDRESS.toRegex())
-                onValidationResult(){ valid ->
+                onValidationResult{ valid ->
                     //TODO: implement
                     if (!valid){
+                        enableuser = false
+                        state.value = enableuser && enablepass
+                        //TODO: Change this
                         Toast.makeText(context,"No empty input",LENGTH_LONG).show()
+                    }else{
+
+                        enableuser = true
+                        state.value = enableuser && enablepass
                     }
                 }
             }
@@ -54,16 +72,35 @@ class LoginFragment : Fragment() {
                 id(2)
                 textHint("Password")
                 regexValidation(Regex("^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{5,}$"))
-                onValidationResult(){ valid ->
+                onValidationResult{ valid ->
                     //TODO: implement
                     if (!valid){
+                        enablepass = false
+                        state.value = enableuser && enablepass
+                        //TODO: Change this
                         Toast.makeText(context,"No empty input",LENGTH_LONG).show()
+                    }else{
+                        enablepass = true
+                        state.value = enableuser && enablepass
                     }
                 }
             }
             epoxyButton{
                 id(3)
                 buttonText("Login")
+                onClickListener{
+
+                }
+                enableButton{ isEnable: MaterialButton ->
+                    lifecycleScope.launch {
+                        viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED){
+                            state.collectLatest {
+                                isEnable.isEnabled = it
+                            }
+                        }
+
+                    }
+                }
             }
 
             epoxyTextButton{
