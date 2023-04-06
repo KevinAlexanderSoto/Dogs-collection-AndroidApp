adb4830277f8bc5901a5411735d955119b733335
Kevin Soto <kevinalexandersoto999@gmail.com>
2023-04-06 14:35:28 -0500

    feature : implement compose in the dog list fragment

--- doglist/src/main/java/com/kalex/dogescollection/doglist/presentation/ui/DogListFragment.kt
+++ doglist/src/main/java/com/kalex/dogescollection/doglist/presentation/ui/DogListFragment.kt
@@ -6,19 +6,19 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.activity.OnBackPressedCallback
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.runtime.*
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.ViewCompositionStrategy
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
-import androidx.recyclerview.widget.GridLayoutManager
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
-import com.google.android.material.progressindicator.LinearProgressIndicator
 import com.kalex.dogescollection.core.common.networkstates.handleViewModelState
 import com.kalex.dogescollection.core.model.data.alldogs.Dog
-import com.kalex.dogescollection.doglist.databinding.DogListFragmentBinding
+import com.kalex.dogescollection.doglist.presentation.composables.DogList
 import com.kalex.dogescollection.doglist.presentation.viewmodel.DogCollectionViewModel
 import com.kalex.dogescollection.doglist.presentation.viewmodel.DogsViewModel
 import dagger.hilt.android.AndroidEntryPoint
-import javax.inject.Inject
 import com.kalex.dogescollection.core.R as coreR
 
 /**
@@ -27,14 +27,8 @@ import com.kalex.dogescollection.core.R as coreR
 @AndroidEntryPoint
 class DogListFragment : Fragment() {
 
-    private var _binding: DogListFragmentBinding? = null
-    private val binding get() = _binding!!
-
     private val dogsViewModel: DogsViewModel by viewModels()
     private val collectionViewModel: DogCollectionViewModel by viewModels()
-
-    @Inject
-    lateinit var dogListAdapter: DogListAdapter
 
     private val backPressedDispatcher = object : OnBackPressedCallback(true) {
         override fun handleOnBackPressed() {
@@ -48,9 +42,15 @@ class DogListFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View {
-        _binding = DogListFragmentBinding.inflate(inflater, container, false)
-        return binding.root
+        return ComposeView(requireContext()).apply {
+            setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
+            setContent {
+                collectionViewModel.getDogCollection()
+                HandleDogsByViewModel()
+            }
+        }
     }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setHasOptionsMenu(true) // Set this to true in order to trigger callbacks to Fragment#onOptionsItemSelected
@@ -59,35 +59,29 @@ class DogListFragment : Fragment() {
             // Redirect system "Back" press to our dispatcher
             onBackPressedDispatcher.addCallback(viewLifecycleOwner, backPressedDispatcher)
         }
-        setUpRecycler(dogListAdapter)
-        binding.linearProgress.indeterminateAnimationType = LinearProgressIndicator.INDETERMINATE_ANIMATION_TYPE_DISJOINT
-        collectionViewModel.getDogCollection()
-
-        handleDogsByViewModel(dogListAdapter)
     }
 
-    private fun setUpRecycler(dogListAdapter: DogListAdapter) {
-        binding.doglistRecycler.layoutManager = GridLayoutManager(context, 2)
-        binding.doglistRecycler.adapter = dogListAdapter
-        dogListAdapter.onItemClick = { dog ->
-            val bundle = DogListFragmentDirections.actionDogListFragmentToDogListDetailFragment(dog)
-            findNavController().navigate(bundle)
+    @Composable
+    private fun HandleDogsByViewModel() {
+        val data = remember {
+            mutableStateListOf<Dog>()
         }
-    }
-
-    private fun handleDogsByViewModel(dogListAdapter: DogListAdapter) {
         handleViewModelState(
             collectionViewModel.getCollectionState,
             onSuccess = {
-                handleSuccessStatus(it, dogListAdapter)
+                data.clear()
+                data += it
             },
             onLoading = {
-                handleLoadingStatus(it)
+                // handleLoadingStatus(it)
             },
             onError = {
                 handleErrorStatus(getString(it))
             },
         )
+        if (data.isNotEmpty()) {
+            handleSuccessStatus(data)
+        }
     }
 
     private fun handleErrorStatus(exception: String) {
@@ -102,16 +96,15 @@ class DogListFragment : Fragment() {
             .show()
     }
 
-    private fun handleSuccessStatus(dogList: List<Dog>, dogListAdapter: DogListAdapter) {
-        dogListAdapter.submitList(dogList)
+    @Composable
+    private fun handleSuccessStatus(dogList: List<Dog>) {
+        DogList(dogList, findNavController())
         handleLoadingStatus(false)
     }
 
     private fun handleLoadingStatus(isLoading: Boolean) {
         if (isLoading) {
-            binding.linearProgress.visibility = View.VISIBLE
         } else {
-            binding.linearProgress.visibility = View.GONE
         }
     }
 
@@ -119,7 +112,6 @@ class DogListFragment : Fragment() {
         // It is optional to remove since our dispatcher is lifecycle-aware. But it wouldn't hurt to just remove it to be on the safe side.
         backPressedDispatcher.remove()
         super.onDestroyView()
-        _binding = null
     }
 
     private fun onBackPressed() {
