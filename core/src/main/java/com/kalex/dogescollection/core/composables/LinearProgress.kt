12a1374a4ef8aaf196e08baa8ffdc0963268dc42
Kevin Soto <kevinalexandersoto999@gmail.com>
2023-04-07 14:13:04 -0500

    feature : add custom linear progress indicator

---
+++ core/src/main/java/com/kalex/dogescollection/core/composables/LinearProgress.kt
@@ -0,0 +1,28 @@
+package com.kalex.dogescollection.core.composables
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.LinearProgressIndicator
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun CustomLinearProgressBar(
+    visibility: Boolean = false,
+) {
+    if (visibility) {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+        ) {
+            LinearProgressIndicator(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(15.dp)
+                    .rotate(-270f),
+
+            )
+        }
+    }
+}
